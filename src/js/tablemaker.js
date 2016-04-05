a0e65247f46a0d017ba4a128df8f963223ee959c
Paceaux <paceaux@madebypaceaux.com>
2016-04-05 09:31:50 -0500

    removed config

--- src/js/tablemaker.js
+++ src/js/tablemaker.js
@@ -1,45 +1,6 @@
 console.clear();
 
-  /*==========
-  #TABLEMAKER #CONFIG
-  ==========*/
 
-/* the tablemaker object is already pretty big. moved the default configs out.
-Besides that, this is an example of what the config MUST look like for building a table from JSON*/
-var tmDefaultConfig = {
-  layout: {
-    header: {
-      rows: 0,
-      cols: 0,
-    },
-    body: {
-      rows: 1,
-      cols: 2,
-    },
-    footer: {
-      rows: 0,
-      cols: 0,
-    },
-    colgroups: 0,
-  },
-  meta: {
-    summary: '',
-    caption: '',
-  },
-  classes: {
-    table: '',
-    rows: {
-      hasEven: false,
-      hasOdd: false,
-      nth: 'none',
-    },
-    cols: {
-      hasEven: false,
-      hasOdd: false,
-      nth: 'none',
-    }
-  },
-};
 
 /*==========
 #TABLEMAKER
